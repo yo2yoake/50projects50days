10441ab829af9d9ebb1007092019222d4b357c7a
YOAKE <yo2yoake@outlook.com>
2024-02-19 15:17:57 +0800

    02-ProgressSteps

---
+++ 02-ProgressSteps/script.js
@@ -0,0 +1,45 @@
+const pregress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
+
+let currentActive = 1;
+
+prev.addEventListener('click', () => {
+    currentActive--;
+    if (currentActive < 1) {
+        currentActive = 1;
+    }
+    console.log(currentActive);
+    update();
+});
+
+next.addEventListener('click', () => {
+    currentActive++;
+    if (currentActive > circles.length) {
+        currentActive = circles.length;
+    }
+    console.log(currentActive);
+    update();
+})
+
+function update() {
+    circles.forEach((circle, index) => {
+        if (index < currentActive) {
+            circle.classList.add("active");
+        } else {
+            circle.classList.remove("active");
+        }
+    })
+
+    progress.style.width = (currentActive - 1) / (circles.length - 1) * 100 + "%";
+
+    if (currentActive == 1) {
+        prev.disabled = true;
+    } else if (currentActive == circles.length) {
+        next.disabled = true;
+    } else {
+        prev.disabled = false;
+        next.disabled = false;
+    }
+}
